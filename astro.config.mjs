5f633899781204ec4ff5f60b06389a69efdc5721
kyeolrii <inskelly@naver.com>
2025-11-21 17:19:19 +0900

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src', // 여기 추가: @ -> src 폴더 매핑
+      },
+    },
   },
-  
   site: 'https://yourdomain.com',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
 });
